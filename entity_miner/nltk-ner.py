e4751d558b003174443c5b0c113494b16d0a6137
sauravns <sauravns@gmail.com>
2019-09-12 23:39:12 +0530

    added entity mining

---
+++ entity_miner/nltk-ner.py
@@ -0,0 +1,17 @@
+import nltk
+
+# Read the file
+f = open('file.txt')
+
+# Each sentence stored in line
+data = f.readlines()
+
+for line in data:
+    # Tokenize each line
+    tokens = nltk.word_tokenize(line)
+    # Apply POS Tagger on tokens
+    tagged = nltk.pos_tag(tokens)
+    # NE chunking on tags
+    entities = nltk.chunk.ne_chunk(tagged)
+    print entities
+f.close()
