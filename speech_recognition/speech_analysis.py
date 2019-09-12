1261601fe33df84e5490f007ec22699e26bd9c83
sauravns <sauravns@gmail.com>
2019-09-12 19:29:51 +0530

    implementing basic tracking algorithms

--- speech_recognition/speech_analysis.py
+++ speech_recognition/speech_analysis.py
@@ -1,10 +1,11 @@
 from hatesonar import Sonar
 import json
-sonar = Sonar ()
 
-data = open("data.txt","r")
-if data.mode == 'r':
-    content = data.read()
-    out = sonar.ping(content)
-    with open('data.json', 'w') as json_file:
-        json.dump(out, json_file)
+def speech_analysis():
+    sonar = Sonar ()
+    data = open("data.txt","r")
+    if data.mode == 'r':
+        content = data.read()
+        out = sonar.ping(content)
+        with open('data.json', 'w') as json_file:
+            json.dump(out, json_file)
